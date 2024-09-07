77927e0f52d353823a7df98af80f29a5a09a0c59
chinm4 <chinm4@rpi.edu>
2024-09-07 16:47:17 -0400

    darker background

--- blackjack-lab/src/App.js
+++ blackjack-lab/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+//import CardBack from '../public/card_imgs/card_back.png';
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Black Jack Lab
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Black Jack Lab
+      </h1>
+    </div>
   );
 }
 
