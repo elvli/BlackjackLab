26e5ee3295930a129f30cd7fe3b62316eeb68611
chinm4 <chinm4@rpi.edu>
2024-09-08 00:04:44 -0400

    boolean for buttons

---
+++ blackjack-lab/src/Components/Dealer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Deck from '../../public/assets/card_imgs/card_back.png';
+
+const Dealer = () => {
+    return (
+        <img src={Deck} alt="deck" />
+    )
+};
+
+export default Dealer;
--- blackjack-lab/src/Components/SettingMenu.js
+++ blackjack-lab/src/Components/SettingMenu.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SettingMenu = () => {
+  const [AceX, SetAceX] = useState(false)
+  const [Pairs, SetPairs] = useState(false)
+  const [Regular, SetRegular] = useState(false)
+  const [Hits17, SetHits17] = useState(false)
+  const [Stands17, SetStands17] = useState(false)
+
+  useEffect(() => {
+    console.log(AceX);
+    console.log(Pairs);
+    console.log(Regular);
+  }, [AceX, Pairs, Regular])
+
+  useEffect(() => {
+    console.log(Hits17);
+    console.log(Stands17);
+  }, [Hits17, Stands17])
+
+  const handleAceX = () => {
+    SetAceX(!AceX);
+    SetPairs(false);
+    SetRegular(false);
+  }
+
+  const handlePairs = () => {
+    SetAceX(false);
+    SetPairs(!Pairs);
+    SetRegular(false);
+  }
+
+  const handleRegular = () => {
+    SetAceX(false);
+    SetPairs(false);
+    SetRegular(!Regular);
+  }
+
+  const handleHit17 = () => {
+    SetHits17(!Hits17);
+    SetStands17(false);
+  }
+
+  const handleStand17 = () => {
+    SetStands17(!Stands17);
+    SetHits17(false);
+  }
+
   return (
     <div>
       <ul
@@ -31,7 +76,7 @@ const SettingMenu = () => {
           className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
         >
           <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer" />
+            <input type="checkbox" value="" class="sr-only peer" onClick={handleAceX} checked={AceX} />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Dealer deals you Ace & X only
@@ -43,7 +88,7 @@ const SettingMenu = () => {
           className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
         >
           <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer" />
+            <input type="checkbox" value="" class="sr-only peer" onClick={handlePairs} checked={Pairs} />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Dealer deals you pairs only
@@ -55,7 +100,7 @@ const SettingMenu = () => {
           className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
         >
           <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer" />
+            <input type="checkbox" value="" class="sr-only peer" onClick={handleRegular} checked={Regular} />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Dealer deals you regulary
@@ -67,7 +112,7 @@ const SettingMenu = () => {
           className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
         >
           <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer" />
+            <input type="checkbox" value="" class="sr-only peer" onClick={handleHit17} checked={Hits17} />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Dealer hits on SOFT 17
@@ -79,7 +124,7 @@ const SettingMenu = () => {
           className="block w-full cursor-pointer select-none rounded-md px-3 pt-[9px] pb-2 text-start leading-tight transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
         >
           <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer" />
+            <input type="checkbox" value="" class="sr-only peer" onClick={handleStand17} checked={Stands17} />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Dealer stands on SOFT 17
