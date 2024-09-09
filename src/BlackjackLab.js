609bdd76a1f45c3ea6109cce3ab4ef9f5d2ffe88
elvenli54 <90856089+elvli@users.noreply.github.com>
2024-09-09 17:59:02 -0400

    Added deck creation and shuffle functions

--- src/BlackjackLab.js
+++ src/BlackjackLab.js
@@ -1,6 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import BlackjackTable from "./Components/BlackjackTable";
+
+function createDeck(numDecks) {
+  if (!numDecks) numDecks = 4;
+
+  const suits = ["D", "S", "H", "C"]; // Diamonds, Spades, Hearts, Clubs
+  const ranks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  const deck = [];
+
+  for (let i = 0; i < numDecks; i++) {
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        deck.push(`${rank}${suit}`);
+      }
+    }
+  }
+
+  return deck;
+}
+
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+
+  return deck;
+}
 
 function App() {
   const [settings, setSettings] = useState({
@@ -11,6 +54,14 @@ function App() {
     Stands17: true,
     Hits17: false,
   });
+
+  useEffect(() => {
+    let deck = createDeck(4);
+    console.log("Deck before shuffle:", deck);
+
+    deck = shuffleDeck(deck);
+    console.log("Deck after shuffle:", deck);
+  }, []);
 
   return (
     <div>
