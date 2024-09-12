e1701f75b2a6c1f931836ae8383adca99d3332cd
elvenli54 <90856089+elvli@users.noreply.github.com>
2024-09-11 20:07:26 -0400

    Hit adds card to my hand, it doesn't react to breaks yet

--- src/Components/BlackjackTable.js
+++ src/Components/BlackjackTable.js
@@ -9,6 +9,7 @@ const BlackjackTable = ({ settings }) => {
   const [Stands17, SetStands17] = useState(settings.Stands17);
   const [Hits17, SetHits17] = useState(settings.Hits17);
 
+  const [deck, setDeck] = useState([]);
   const [mySum, setMySum] = useState(0);
   const [dealerSum, setDealerSum] = useState(0);
   const [myAceCount, setMyAceCount] = useState(0);
@@ -18,9 +19,10 @@ const BlackjackTable = ({ settings }) => {
   const [canHit, setCanHit] = useState(true);
   const [dealerCanHit, setDealerCanHit] = useState(true);
 
-  let deck = [];
+  // let deck = [];
 
   const createDeck = (numDecks) => {
+    const newDeck = [];
     if (!numDecks) numDecks = 4;
     const suits = ["D", "S", "H", "C"];
     const ranks = [
@@ -42,26 +44,35 @@ const BlackjackTable = ({ settings }) => {
     for (let i = 0; i < numDecks; i++) {
       for (let suit of suits) {
         for (let rank of ranks) {
-          deck.push(rank + "-" + suit);
+          newDeck.push(rank + "-" + suit);
         }
       }
     }
+    setDeck(newDeck);
   };
 
   const shuffleDeck = () => {
-    for (let i = deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
+    setDeck((prevDeck) => {
+      const shuffledDeck = [...prevDeck];
+      for (let i = shuffledDeck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+      }
+      return shuffledDeck;
+    });
   };
 
-  const getValue = (card) => {
+  const getValue = (card, currentSum = 0) => {
     let data = card.split("-");
     let value = data[0];
+
     if (isNaN(value)) {
-      if (value === "A") return 11;
+      if (value === "A") {
+        return currentSum + 11 > 21 ? 1 : 11;
+      }
       return 10;
     }
+
     return parseInt(value);
   };
 
@@ -78,38 +89,80 @@ const BlackjackTable = ({ settings }) => {
     return playerSum;
   };
 
+  const hit = () => {
+    setDeck((prevDeck) => {
+      let card = prevDeck.pop();
+      console.log(prevDeck.length);
+      let newMyCards = [...myCards, card];
+      let myNewSum = mySum + getValue(card);
+      let myNewAceCount = myAceCount + checkAce(card);
+      myNewSum = reduceAce(myNewSum, myNewAceCount);
+
+      setMyCards(newMyCards);
+      setMySum(myNewSum);
+      setMyAceCount(myNewAceCount);
+
+      setCanHit(myNewSum < 21);
+
+      console.log("MY SUM:", myNewSum);
+      if (myNewSum > 21) console.log("BREAK");
+      return prevDeck;
+    });
+  };
+
   const startGame = () => {
-    let card = deck.pop();
-    let newMyCards = [card];
-    let myNewSum = getValue(card);
-    let myNewAceCount = checkAce(card);
+    setDeck((prevDeck) => {
+      let card = prevDeck.pop();
+      let newMyCards = [card];
+      let myNewSum = getValue(card);
+      let myNewAceCount = checkAce(card);
 
-    card = deck.pop();
-    let newDealerCards = [card];
-    let dealerNewSum = getValue(card);
-    let dealerNewAceCount = checkAce(card);
+      card = prevDeck.pop();
+      let newDealerCards = [card];
+      let dealerNewSum = getValue(card);
+      let dealerNewAceCount = checkAce(card);
 
-    card = deck.pop();
-    newMyCards.push(card);
-    myNewSum += getValue(card);
-    myNewAceCount += checkAce(card);
-    myNewSum = reduceAce(myNewSum, myNewAceCount);
+      card = prevDeck.pop();
+      newMyCards.push(card);
+      myNewSum += getValue(card);
+      myNewAceCount += checkAce(card);
+      myNewSum = reduceAce(myNewSum, myNewAceCount);
 
-    card = deck.pop();
-    newDealerCards.push(card);
-    dealerNewSum += getValue(card);
-    dealerNewAceCount += checkAce(card);
-    dealerNewSum = reduceAce(dealerNewSum, dealerNewAceCount);
+      card = prevDeck.pop();
+      newDealerCards.push(card);
+      dealerNewSum += getValue(card);
+      dealerNewAceCount += checkAce(card);
+      dealerNewSum = reduceAce(dealerNewSum, dealerNewAceCount);
 
-    setMyCards(newMyCards);
-    setMySum(myNewSum);
-    setMyAceCount(myNewAceCount);
-    setDealerCards(newDealerCards);
-    setDealerSum(dealerNewSum);
-    setDealerAceCount(dealerNewAceCount);
+      setMyCards(newMyCards);
+      setMySum(myNewSum);
+      setMyAceCount(myNewAceCount);
+      setDealerCards(newDealerCards);
+      setDealerSum(dealerNewSum);
+      setDealerAceCount(dealerNewAceCount);
 
-    setCanHit(!(myNewSum === 21));
-    setDealerCanHit(!(dealerNewSum === 21));
+      setCanHit(myNewSum < 21);
+      setDealerCanHit(dealerNewSum < 17);
+
+      return prevDeck;
+    });
+  };
+
+  const handleHit = () => {
+    console.log("deck:", deck);
+    hit();
+  };
+
+  const handleStand = () => {
+    console.log("Stand button clicked");
+  };
+
+  const handleDouble = () => {
+    console.log("Double button clicked");
+  };
+
+  const handleSplit = () => {
+    console.log("Split button clicked");
   };
 
   useEffect(() => {
@@ -118,6 +171,7 @@ const BlackjackTable = ({ settings }) => {
     startGame();
 
     console.log("deck num:", NumDecks);
+    console.log("deck:", deck);
   }, []);
 
   return (
@@ -141,6 +195,7 @@ const BlackjackTable = ({ settings }) => {
         <button
           type="button"
           className="text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-500 dark:hover:bg-blue-600"
+          onClick={handleHit}
         >
           Hit
         </button>
@@ -148,6 +203,7 @@ const BlackjackTable = ({ settings }) => {
         <button
           type="button"
           className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-500 dark:hover:bg-red-600"
+          onClick={handleStand}
         >
           Stand
         </button>
@@ -155,6 +211,7 @@ const BlackjackTable = ({ settings }) => {
         <button
           type="button"
           className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700"
+          onClick={handleDouble}
         >
           Double
         </button>
@@ -162,6 +219,7 @@ const BlackjackTable = ({ settings }) => {
         <button
           type="button"
           className="text-white bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700"
+          onClick={handleSplit}
         >
           Split
         </button>
